a4e5819ae8c552504c80c1e049d76bb1a8f9e5ec
rachele <rachelecerasoli6@gmail.com>
2024-02-05 16:21:26 +0100

    Aggiornamento

--- es-31/exercise.js
+++ es-31/exercise.js
@@ -1,26 +1,10 @@
-// function printName(name) {
-//   console.log(name);
-// }
-
-// function sayHelloName(callback) {
-//   console.log("Hello");
-//   callback("Cosimo"); // Passaggio di un nome (può essere qualsiasi nome) alla funzione printName
-// }
-
-// // Chiamata alla funzione sayHelloName con printName come callback
-// sayHelloName(printName);
-
-
-
-
-
 function printName(name) {
     console.log(name);
 }
 
 function sayHelloName(callback) {
     console.log('Ciao');
-    
+
     callback('Cosimo');
 }
 
