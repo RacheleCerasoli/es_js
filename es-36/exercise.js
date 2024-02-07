375541ecdcea6937470c8dc1d81cb3d27eee3bdc
rachele <rachelecerasoli6@gmail.com>
2024-02-07 14:46:04 +0100

    Esercizi 35-36

---
+++ es-36/exercise.js
@@ -0,0 +1,30 @@
+// function canPlay() {
+//   let sport = " Football";
+
+//   if (true) {
+//     let personName = "Cosimo";
+//   }
+
+//   console.log(personName + sport);
+// }
+
+// canPlay();
+
+
+
+let sport;
+let personName;
+
+function canPlay() {
+  sport = " Football";
+
+  if (true) {
+    personName = "Cosimo";
+
+  }
+
+  console.log(personName + " " + sport);
+
+}
+
+canPlay();
